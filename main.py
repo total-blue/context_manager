e4882f94788fef1aa2b7ab05950c64aa6060ad77
Polina Prikhodova <polinaprikhodova@gmail.com>
2020-01-26 16:02:44 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+import datetime
+class OpenerCounter:
+    def __enter__(self):
+        self.enter_time = datetime.datetime.now()
+        print('Entry time ', self.enter_time)
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.exit_time = datetime.datetime.now()
+        print('Exit time ', self.exit_time)
+        self.res = self.exit_time - self.enter_time
+        print('Spent: ', self.res)
+
+if __name__ == '__main__':
+    with OpenerCounter():
+        print('test')
